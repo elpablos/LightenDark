6c89265634378b17675cf43639736552e2d6b97a
elpablos <elpablos@gmail.com>
2016-01-19 11:38:32 +0100

    finded lost "children"

---
+++ LightenDark.Api/Interfaces/ICore.cs
@@ -0,0 +1,15 @@
+﻿using LightenDark.Api.Args;
+using LightenDark.Api.Enums;
+using System;
+
+namespace LightenDark.Api.Interfaces
+{
+    public interface ICore
+    {
+        event EventHandler<MessageEventArgs> MessageIncome;
+
+        void LogNewAction(ApplicationMessageType type, string msg);
+
+        void JavacriptAction(object param);
+    }
+}
