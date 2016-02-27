4235481afaba04bc963110ceccece78076be5991
elpablos <elpablos@gmail.com>
2016-02-27 10:19:21 +0100

    new version 1.3.x

--- LightenDark.Studio/Properties/AssemblyInfo.cs
+++ LightenDark.Studio/Properties/AssemblyInfo.cs
@@ -51,5 +51,5 @@ using System.Windows.Markup;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.2.*")]
+[assembly: AssemblyVersion("1.3.*")]
 [assembly: XmlnsDefinition("http://lightendark.lorenzo.cz/", "LightenDark")]
